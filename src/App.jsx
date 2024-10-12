2ef95cbe261f5cb197f2244b2312826a3a4fbab9
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2024-10-12 17:59:42 +0200

    initial commit before continuous integration

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+export default function App() {
+	return (
+		<BrowserRouter>
+			<header>
+				<Link
+					className="site-logo"
+					to="/"
+				>
+					#VANLIFE
+				</Link>
+				<nav>
+					<Link to="/about">About</Link>
+				</nav>
+			</header>
+			<Routes>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
+				<Route
+					path="/about"
+					element={<About />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
+}
